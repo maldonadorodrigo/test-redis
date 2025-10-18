b16a30ff6b010ecf7cf9d1193c9b94ddaaf74783
Rodrigo Maldonado <90664511+maldonadorodrigo@users.noreply.github.com>
2025-10-18 11:45:40 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import redis
+import os
+import time
+
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+
+while True:
+    try:
+        r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT)
+        r.ping()
+        print(f"✅ Conectado ao Redis em {REDIS_HOST}:{REDIS_PORT}")
+    except redis.ConnectionError as e:
+        print(f"❌ Falha na conexão: {e}")
+    time.sleep(3)
